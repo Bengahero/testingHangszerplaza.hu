44e4961729df5a156d1fd2ea42e40c7e3e0de735
Bengahero <64478533+Bengahero@users.noreply.github.com>
2021-08-23 13:24:57 +0200

    update

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -34,7 +34,7 @@ public class BaseTest {
         options.addArguments("--disable-extensions");
         // options.addArguments("start-maximized");
        // options.addArguments("--disable-dev-shm-usage");
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         //options.addArguments("--proxy-server='direct://");
         //options.addArguments("--proxy-bypass-list=*");
         options.addArguments("--disable-gpu");
